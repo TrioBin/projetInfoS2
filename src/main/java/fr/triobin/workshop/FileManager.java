74e9976ebdedbbf4f59e1b4e4741b7b27efdc75f
FatlumAL <fatlum.muzaqi@insa-strasbourg.fr>
2025-05-07 10:52:44 +0200

    Remove Gson dependency from FileManager class

--- src/main/java/fr/triobin/workshop/FileManager.java
+++ src/main/java/fr/triobin/workshop/FileManager.java
@@ -4,16 +4,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 public class FileManager {
     public static ArrayList<Workshop> loadFile() {
         return null; // TODO: Implement file loading logic
     }
 
     public static void saveFile(ArrayList<Workshop> workshop) {
-        final GsonBuilder builder = new GsonBuilder();
-        final Gson gson = builder.create();
     }
 }
