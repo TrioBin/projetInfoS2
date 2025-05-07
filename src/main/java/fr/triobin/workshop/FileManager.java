1e49308bda2576c1ae7e6670818c8f137a441871
Ascol57 <constant.rusche@gmail.com>
2025-05-07 11:28:04 +0200

    Add Jackson dependencies and update FileManager for JSON handling

--- src/main/java/fr/triobin/workshop/FileManager.java
+++ src/main/java/fr/triobin/workshop/FileManager.java
@@ -5,20 +5,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FileManager {
     public static ArrayList<Workshop> loadFile() {
+        ObjectMapper mapper = new ObjectMapper();
         return null; // TODO: Implement file loading logic
     }
 
     public static void saveFile(ArrayList<Workshop> workshop) {
-       try (BufferedWriter writer = new BufferedWriter(new FileWriter("workshops.csv"))) {
-            for (Workshop w : workshop) {
-                writer.write(w.toString());
-                writer.newLine();
-            }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("workshops.csv"))) {
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(null);
         } catch (IOException e) {
-            e.printStackTrace(); 
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module fr.triobin.workshop {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.sql;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
     opens fr.triobin.workshop to javafx.fxml;
     exports fr.triobin.workshop;
