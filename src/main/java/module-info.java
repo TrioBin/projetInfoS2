e74ee18936dae4d3631553f3cca39b12a9a5e53b
Ascol57 <constant.rusche@gmail.com>
2025-05-07 10:51:25 +0200

    Remove Gson dependency from module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module fr.triobin.workshop {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.sql;
-    requires com.google.gson;
 
     opens fr.triobin.workshop to javafx.fxml;
     exports fr.triobin.workshop;
