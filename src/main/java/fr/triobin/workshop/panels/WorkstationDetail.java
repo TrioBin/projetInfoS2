102333be4f0457eb26314face35cdc54fe68ffa6
Ascol57 <constant.rusche@gmail.com>
2025-05-13 19:32:04 +0200

    feat: Refactor WorkstationPanel and add WorkstationDetail panel for improved workstation management

---
+++ src/main/java/fr/triobin/workshop/panels/WorkstationDetail.java
@@ -0,0 +1,16 @@
+package fr.triobin.workshop.panels;
+
+import fr.triobin.workshop.customgui.CustomPanel;
+import fr.triobin.workshop.general.Workstation;
+import javafx.stage.Stage;
+
+public class WorkstationDetail extends CustomPanel {
+    public WorkstationDetail(Workstation workstation) {
+        super();
+    }
+
+    @Override
+    public void onload(Stage stage) {
+        // Custom behavior on load can be added here
+    }
+}
--- src/main/java/fr/triobin/workshop/panels/WorkstationPanel.java
+++ src/main/java/fr/triobin/workshop/panels/WorkstationPanel.java
@@ -1,15 +1,13 @@
 package fr.triobin.workshop.panels;
 
 import fr.triobin.workshop.Memory;
-import fr.triobin.workshop.customgui.CustomCapacities;
 import fr.triobin.workshop.customgui.CustomPanel;
 import fr.triobin.workshop.customgui.Modal;
-import fr.triobin.workshop.popups.CreateWorkstationPopup;
+import fr.triobin.workshop.popups.CreateOperatorPopup;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -19,94 +17,65 @@ public class WorkstationPanel extends CustomPanel {
 
     public WorkstationPanel() {
         super();
-        // Espacements généraux
-        this.setSpacing(20);
+        this.setSpacing(0);
+        this.setPadding(new Insets(0));
         this.setStyle("-fx-background-color: white;");
 
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setPadding(new Insets(0, 40, 0, 40));
+        // Partie droite (grande zone grise)
+        Pane rightPane = new Pane();
+        rightPane.setStyle("-fx-background-color: lightgray; -fx-border-color: black;");
+        HBox.setHgrow(rightPane, Priority.ALWAYS);
 
-        // Grid 2 lignes × 3 colonnes
-        GridPane grid = new GridPane();
-        grid.setHgap(30);
-        grid.setVgap(30);
+        // Partie gauche (zone orangée)
+        VBox leftPane = new VBox(10);
+        leftPane.setPadding(new Insets(20));
+        leftPane.setPrefWidth(300);
+        leftPane.setStyle("-fx-background-color: #facc7c; -fx-border-color: #2196f3; -fx-border-width: 2px;");
 
-        int i = 0;
-        // Créer les 5 cartes postes
-        for (i = 0; i < Memory.currentWorkshop.getWorkstations().size(); i++) {
-            StackPane poste = createPoste(Memory.currentWorkshop.getWorkstations().get(i).getRefWorkstation());
-            poste.setPrefSize(300, 100);
-            grid.add(poste, i % 3, i / 3);
-        }
+        Label titleButton = new Label("Postes de travail");
+        titleButton.setMaxWidth(Double.MAX_VALUE);
+        leftPane.getChildren().add(titleButton);
 
-        // La carte “+” en sixième position
-        StackPane addCard = createAddCard();
-        addCard.setPrefSize(300, 100);
-        grid.add(addCard, i % 3, i / 3);
+        Memory.currentWorkshop.getWorkstations().forEach(workstation -> {
+            Button workstationButton = new Button(workstation.getRefWorkstation());
+            workstationButton.setMaxWidth(Double.MAX_VALUE);
+            leftPane.getChildren().add(workstationButton);
 
-        scrollPane.setContent(grid);
-        scrollPane.setFitToWidth(true);
-
-        this.getChildren().add(scrollPane);
-    }
-
-    private StackPane createPoste(String title) {
-        StackPane pane = new StackPane();
-        pane.setPrefSize(300, 200);
-        pane.setStyle("-fx-background-color: #FFD68D; -fx-border-color: black;");
-
-        Label label = new Label(title);
-        label.setFont(Font.font(20));
-        pane.getChildren().add(label);
-        StackPane.setAlignment(label, Pos.CENTER);
-
-        CustomCapacities.hoverCursorEffect(pane, Cursor.HAND);
-
-        // Clique éventuel
-        pane.setOnMouseClicked(e -> {
-            System.out.println("Poste sélectionné : " + title);
-            // TODO → navigation vers la vue détaillée du poste
+            workstationButton.setOnAction(actionEvent -> {
+                rightPane.getChildren().clear();
+                rightPane.getChildren().add(new WorkstationDetail(workstation));
+            });
         });
+        Button addButton = new Button("+");
+        addButton.setMaxWidth(Double.MAX_VALUE);
 
-        return pane;
-    }
+        addButton.setOnAction(event -> {
+            // Create a modal
+            Modal dialog = new Modal(this.stage, new CreateOperatorPopup());
+            dialog.onClose(closeEvent -> {
+                ((VBox) this.getChildren().get(0)).getChildren().clear();
+                ((VBox) this.getChildren().get(0)).getChildren().add(titleButton);
+                Memory.currentWorkshop.getWorkstations().forEach(workstation -> {
+                    Button workstationButton = new Button(workstation.getRefWorkstation());
+                    workstationButton.setMaxWidth(Double.MAX_VALUE);
+                    leftPane.getChildren().add(workstationButton);
 
-    private StackPane createAddCard() {
-        StackPane pane = new StackPane();
-        pane.setPrefSize(300, 200);
-        pane.setStyle("-fx-background-color: #FFD68D; -fx-border-color: black;");
-
-        Label plus = new Label("+");
-        plus.setFont(Font.font(60));
-        pane.getChildren().add(plus);
-        StackPane.setAlignment(plus, Pos.CENTER);
-
-        CustomCapacities.hoverCursorEffect(pane, Cursor.HAND);
-
-        pane.setOnMouseClicked(e -> {
-            Modal dialog = new Modal(this.stage, new CreateWorkstationPopup());
-            dialog.onClose(o -> {
-                // reload grid
-                GridPane gridPane = (GridPane) ((ScrollPane) this.getChildren().get(0)).getContent();
-                gridPane.getChildren().clear();
-                int i = 0;
-                for (i = 0; i < Memory.currentWorkshop.getWorkstations().size(); i++) {
-                    StackPane poste = createPoste(Memory.currentWorkshop.getWorkstations().get(i).getRefWorkstation());
-                    poste.setPrefSize(300, 100);
-                    gridPane.add(poste, i % 3, i / 3);
-                }
-                StackPane addCard = createAddCard();
-                addCard.setPrefSize(300, 100);
-                gridPane.add(addCard, i % 3, i / 3);
+                    workstationButton.setOnAction(actionEvent -> {
+                        rightPane.getChildren().clear();
+                        rightPane.getChildren().add(new WorkstationDetail(workstation));
+                    });
+                });
+                ((VBox) this.getChildren().get(0)).getChildren().add(addButton);
             });
         });
 
-        return pane;
+        leftPane.getChildren().add(addButton);
+
+        this.getChildren().addAll(leftPane, rightPane);
     }
 
     @Override
     public void onload(Stage stage) {
         this.stage = stage;
-        stage.setTitle("- Workstation");
     }
 }
